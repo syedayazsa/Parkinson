e4dcfa4d2f17aee97d6fc26fac36ea836c449c85
Syed Ayaz <51998832+syedayazsa@users.noreply.github.com>
2019-12-14 16:11:28 +0530

    Add files via upload

---
+++ Parkinson/parkinson.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 15:59:48 2019
+
+@author: ayaz
+"""
+
+import numpy as np
+import pandas as pd
+import os, sys
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df=pd.read_csv('parkinsons.data')
+df.head()
+
+
+#X and Y
+X = df.loc[:,df.columns!='status'].values[:,1:]
+y = labels=df.loc[:,'status'].values
+
+#Feature Scaling
+scaler=MinMaxScaler((-1,1))
+x=scaler.fit_transform(X)
+
+#Splitting
+x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=0)
+
+#Model training
+model=XGBClassifier()
+model.fit(x_train,y_train)
+
+#Prediction
+y_pred=model.predict(x_test)
+print(accuracy_score(y_test, y_pred)*100)
